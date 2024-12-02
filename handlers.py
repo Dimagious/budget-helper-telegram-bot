80f2a187d8936d0f5bc9ee02e74addbed91dbfff
Dmitriy <dmitriyyurkin-aq@team.asana.com>
2024-12-02 15:58:39 +0100

    added logic to get categories from the context

--- handlers.py
+++ handlers.py
@@ -52,6 +52,9 @@ async def handle_category(update: Update, context: CallbackContext) -> int:
         # Клавиатура для категорий расходов
         expense_categories = gs_helper.get_categories(constants.EXPENSE_SHEET_NAME, constants.RANGE_FOR_EXPENSE_CATEGORIES)
         keyboard = create_keyboard(expense_categories, True)
+        chosen_category = update.message.text
+        if chosen_category in expense_categories:
+            context.user_data['category'] = chosen_category
         await update.message.reply_text("Выбери категорию расхода:", reply_markup=keyboard)
         return constants.SET_EXPENSE_CATEGORY
     else:
@@ -65,7 +68,6 @@ async def add_income(update: Update, context: CallbackContext) -> None:
     
     # Получение категорий доходов
     categories = gs_helper.get_categories(constants.INCOME_SHEET_NAME, constants.RANGE_FOR_INCOME_CATEGORIES)
-    context.user_data['categories'] = categories
     chosen_category = update.message.text
     if chosen_category in categories:
         context.user_data['category'] = chosen_category
@@ -82,7 +84,6 @@ async def add_expense(update: Update, context: CallbackContext) -> int:
 
     # Получение категорий расходов
     categories = gs_helper.get_categories(constants.EXPENSE_SHEET_NAME, constants.RANGE_FOR_EXPENSE_CATEGORIES)
-    context.user_data['categories'] = categories
     chosen_category = update.message.text
     if chosen_category in categories:
         context.user_data['category'] = chosen_category
@@ -96,21 +97,24 @@ async def add_expense(update: Update, context: CallbackContext) -> int:
 async def set_expense_amount(update: Update, context: CallbackContext) -> int:
     """Обработка ввода суммы и обновление Google Sheets."""
     try:
-        # Извлекаем категорию из user_data
-        category = context.user_data.get('category')
-        if not category:
+        # Извлечение категории из контекста
+        chosen_category = context.user_data.get('category', 'Неизвестная категория')
+
+        # Получение категорий из Google Sheets
+        categories = gs_helper.get_categories(constants.EXPENSE_SHEET_NAME, constants.RANGE_FOR_EXPENSE_CATEGORIES)
+        if not chosen_category in categories:
             await update.message.reply_text(constants.CHOOSE_CATEGORY)
             return constants.SET_EXPENSE_CATEGORY
 
         # Удаляем запятую или заменяем её на точку для корректной обработки
-        expense_str = update.message.text.replace(',', '').strip()
-        if not expense_str.isdigit():
+        expense_str = update.message.text.replace(',', '.').strip()
+        if not expense_str.replace('.', '', 1).isdigit():  # Проверка на число с десятичной точкой
             await update.message.reply_text(constants.WRONG_AMOUNT)
             return constants.SET_EXPENSE_AMOUNT
 
         expense = float(expense_str)  # Преобразуем строку в число
 
-        row_to_update = gs_helper.get_categories(constants.EXPENSE_SHEET_NAME, constants.RANGE_FOR_EXPENSE_CATEGORIES).get(category)
+        row_to_update = categories.index(chosen_category) + 2  # Строка для обновления
         col_to_update = get_current_month_column()
 
         existing_value_str = gs_helper.get_cell_value(row_to_update, col_to_update, constants.EXPENSE_SHEET_NAME)
@@ -119,9 +123,9 @@ async def set_expense_amount(update: Update, context: CallbackContext) -> int:
 
         gs_helper.update_cell(row_to_update, col_to_update, updated_value, constants.EXPENSE_SHEET_NAME)
         user_name = update.message.from_user.username
-        logging.info(f"{user_name} внёс расход {expense} в \"{category}\". Было: {existing_value}. Стало: {updated_value}")
+        logging.info(f"{user_name} внёс расход {expense} в \"{chosen_category}\". Было: {existing_value}. Стало: {updated_value}")
 
-        await update.message.reply_text(constants.EXPENSE_ADDED.format(category, expense))
+        await update.message.reply_text(constants.EXPENSE_ADDED.format(chosen_category, expense))
     except (ValueError, AttributeError) as e:
         logging.error(f"Ошибка обновления: {e}")
         await update.message.reply_text(constants.ERROR_PROCESSING)
@@ -131,21 +135,25 @@ async def set_expense_amount(update: Update, context: CallbackContext) -> int:
 async def set_income_amount(update: Update, context: CallbackContext) -> int:
     """Обработка ввода суммы и обновление Google Sheets."""
     try:
-        # Извлекаем категорию из user_data
-        category = context.user_data.get('category')
-        if not category:
+        # Извлечение категории из контекста
+        chosen_category = context.user_data.get('category', 'Неизвестная категория')
+        logging.info(f"Извлекаем категория из контекста: {chosen_category}")
+        
+        # Получение категорий из Google Sheets
+        categories = gs_helper.get_categories(constants.INCOME_SHEET_NAME, constants.RANGE_FOR_INCOME_CATEGORIES)
+        if not chosen_category in categories:
             await update.message.reply_text(constants.CHOOSE_CATEGORY)
             return constants.SET_INCOME_CATEGORY
         
         # Удаляем запятую или заменяем её на точку для корректной обработки
-        income_str = update.message.text.replace(',', '').strip()
-        if not income_str.isdigit():
+        income_str = update.message.text.replace(',', '.').strip()
+        if not income_str.replace('.', '', 1).isdigit():  # Проверка на число с десятичной точкой
             await update.message.reply_text(constants.WRONG_AMOUNT)
             return constants.SET_INCOME_AMOUNT
-        
+
         income = float(income_str)  # Преобразуем строку в число
         
-        row_to_update = gs_helper.get_categories(constants.INCOME_SHEET_NAME, constants.RANGE_FOR_INCOME_CATEGORIES).get(category)
+        row_to_update = categories.index(chosen_category) + 3  # Строка для обновления
         col_to_update = get_current_month_column()
         
         existing_value_str = gs_helper.get_cell_value(row_to_update, col_to_update, constants.INCOME_SHEET_NAME)
@@ -154,9 +162,9 @@ async def set_income_amount(update: Update, context: CallbackContext) -> int:
         
         gs_helper.update_cell(row_to_update, col_to_update, updated_value, constants.INCOME_SHEET_NAME)
         user_name = update.message.from_user.username
-        logging.info(f"{user_name} внёс доход {income} в \"{category}\". Было: {existing_value}. Стало: {updated_value}")
+        logging.info(f"{user_name} внёс доход {income} в \"{chosen_category}\". Было: {existing_value}. Стало: {updated_value}")
         
-        await update.message.reply_text(constants.INCOME_ADDED.format(category, income))
+        await update.message.reply_text(constants.INCOME_ADDED.format(chosen_category, income))
     except (ValueError, AttributeError) as e:
         logging.error(f"Ошибка обновления: {e}")
         await update.message.reply_text(constants.ERROR_PROCESSING)
